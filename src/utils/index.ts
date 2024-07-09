2f60a831f66d70847a3dbc9e207439b239de37c1
Tomiwa Adelae <thetommedia@gmail.com>
2024-07-09 14:18:12 +0100

    Updated the link

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://localhost:9000";
+// export const BACKEND_URL = "http://localhost:9000";
+export const BACKEND_URL = "https://online-voting-backend-bnew.onrender.com";
